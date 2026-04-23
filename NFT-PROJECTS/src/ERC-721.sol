1422157c9893d0820def15c19e15dcbeb5a5461b
yashu780 <sharmayashu740@gmail.com>
2026-04-23 21:46:20 +0530

    Auto update Thu, Apr 23, 2026  9:46:19 PM

--- NFT-PROJECTS/src/ERC-721.sol
+++ NFT-PROJECTS/src/ERC-721.sol
@@ -9,12 +9,15 @@ import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ER
 
 contract Web3erc721 is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
     uint256 private _nextTokenId;
-    uint  public maxsupply ;
+    uint256  public maxsupply = 5000;
+    bool public publicMintopen = false;
+    bool public allowlistmintopen = false;
+    mapping(address => bool) public Allowlistmint;
 
     constructor()
     ERC721("web3erc721", "we3")
     Ownable(msg.sender)
-{}
+    {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
@@ -28,15 +31,41 @@ contract Web3erc721 is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         _unpause();
     }
 
-    function publicMint() public payable returns(uint256){
-        require(msg.value == 0.01 ether, "NOT ENOUGH FUNDS");
-        require(totalSupply() < maxsupply,"WE SOLD OUT");
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(msg.sender, tokenId);
-       return tokenId;
+    function editmintwindow(bool _publicMintopen,bool _allowlistmintopen) external onlyOwner{
+        publicMintopen =_publicMintopen;
+        allowlistmintopen =_allowlistmintopen;
     }
 
+    function allowlistmint() public payable{
+        require(allowlistmintopen,"ALLOW LIST MINT CLOSED");
+        require(Allowlistmint[msg.sender],"YOU ARE NOT IN ALLOW LIST ");
+        require(msg.value == 0.1 ether,"NOT ENOUGH FUNDS");
+        internalmint();
+    }
+
+    function publicMint() public payable returns(uint256){
+        require(publicMintopen,"PUBLICLIST MINT CLOSED");
+        require(msg.value == 0.01 ether, "NOT ENOUGH FUNDS");
+        return internalmint();
+    }
+
+    function internalmint() internal returns(uint256){
+        require(totalSupply()< maxsupply,"WE SOLD OUT");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        return tokenId;
+    }
+    function withdraw(address _addr) external onlyOwner{
+        uint256 balanlce = address(this).balance;
+        payable(_addr).transfer(balanlce);
+    } 
+
     // The following functions are overrides required by Solidity.
+    function setAllowlist(address[] calldata addresses)external onlyOwner{
+       for(uint256 i=0; i< addresses.length; i++){
+        Allowlistmint[addresses[i]] = true;
+       } 
+    }
 
     function _update(address to, uint256 tokenId, address auth)
         internal
