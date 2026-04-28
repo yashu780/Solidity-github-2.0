cfff73697f645956c3da01a9741d25e5e83543a7
yashu780 <sharmayashu740@gmail.com>
2026-04-28 22:04:05 +0530

    Auto update Tue, Apr 28, 2026 10:04:05 PM

--- erc1155-PROJECT/src/contract.sol
+++ erc1155-PROJECT/src/contract.sol
@@ -6,10 +6,16 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+//import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract WEB3builder is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
-    uint256 public publicprice = 0.01 ether;
+    uint256 public publicprice = 0.05 ether;
+    uint256 public allowListprice = 0.01 ether;
     uint256 public maxsupply = 5000;
+    bool public publicMintopen = false;
+    bool public allowListopen= true;
+     mapping(address=> bool) allowList;
     constructor(address initialOwner)
         ERC1155("ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/")
         Ownable(initialOwner)
@@ -17,6 +23,15 @@ contract WEB3builder is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
+    }
+     function uri(uint256 _id) public view virtual override returns (string memory) {
+        require(exists(_id), "URI: nonexistent token");
+        
+        return string(abi.encodePacked(super.uri(_id), Strings.toString(_id), ".json"));
+    }
+       function withdraw(address _addr) external onlyOwner {
+        uint256 balance = address(this).balance; // get the balance of the smart contract
+        payable(_addr).transfer(balance);
     }
 
     function pause() public onlyOwner {
@@ -26,13 +41,28 @@ contract WEB3builder is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
     function unpause() public onlyOwner {
         _unpause();
     }
+    function allowList(uint256 id, uint256 amount) public payable{
+        require(allowListopen,"ALLOW LIST MINT IS CLOSED");
+       require(msg.value == allowListprice * amount,"Insufficient funds");
+        require(id < 2,"Invalid token ID");
+        require(allowList[msg.sender],"YOU ARE NOT IN THE ALLOWLIST");
+        require(totalSupply(id) + amount<= maxsupply,"WE SOLD OUT");
+        _mint(msg.sender, id, amount,"");
+
+    }
+    function setAllowlist( address[] calldata addresses) external onlyOwner{
+        for(uint256 i=0; i< addresses.length; i++){
+            allowList[addressses[i]] = true;
+        }
+    }
 
     function mint( uint256 id, uint256 amount)
         public payable
         
     {
-        require(msg.value == publicprice* amount,"Insufficient funds");
-        require(id <2,"Invalid token ID");
+        require(publicMintopen, "PUBLIC MINT CLOSED");
+        require(msg.value == publicprice * amount,"Insufficient funds");
+        require(id < 2,"Invalid token ID");
         require(totalSupply(id) + amount<= maxsupply,"WE SOLD OUT");
         _mint(msg.sender, id, amount,"");
     }
@@ -45,7 +75,13 @@ contract WEB3builder is ERC1155, Ownable, ERC1155Pausable, ERC1155Supply {
     }
 
     // The following functions are overrides required by Solidity.
-
+       function editMintWindows(
+        bool _publicMintopen;
+        bool _allowListopen;
+       ) external onlyOwner {
+        publicMintopen = _publicMintopen;
+        allowListopen = _allowListopen;
+       }
     function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
         internal
         override(ERC1155, ERC1155Pausable, ERC1155Supply)
